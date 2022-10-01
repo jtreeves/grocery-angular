9a8040a7247f206b820e9057d69c65b96835790f
jtreeves <jackson.t.reeves@gmail.com>
2022-10-01 17:21:17 -0400

    Create initial folders and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoryResultsComponent } from './category-results/category-results.component';
+import { CheckoutItemComponent } from './checkout-item/checkout-item.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductHighlightComponent } from './product-highlight/product-highlight.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ReceiptComponent } from './receipt/receipt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CategoryResultsComponent,
+    CheckoutItemComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductHighlightComponent,
+    ProductsListComponent,
+    ReceiptComponent
   ],
   imports: [
     BrowserModule,
