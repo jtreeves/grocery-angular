432c4af142684b291d1c4d3ce0fc77cf51af9072
jtreeves <jackson.t.reeves@gmail.com>
2022-10-01 21:06:47 -0400

    [app-routing] Add routes for different pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { HomeComponent } from './home/home.component'
+import { CategoryResultsComponent } from './category-results/category-results.component'
+import { CartComponent } from './cart/cart.component'
+import { ReceiptComponent } from './receipt/receipt.component'
 
-const routes: Routes = []
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'results',
+        component: CategoryResultsComponent
+    },
+    {
+        path: 'cart',
+        component: CartComponent
+    },
+    {
+        path: 'receipt',
+        component: ReceiptComponent
+    }
+]
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
