369ddcd3dc3e11bf01574b670002c95ab7ee82a5
jtreeves <jackson.t.reeves@gmail.com>
2022-10-04 18:19:58 -0400

    [services] Mirror stores in Vue

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core'
+import { ProductTally } from '../interfaces/product-tally.interface'
+import { findProductInCollection } from '../utilities/find-product-in-collection.utility'
+import { updateProductTally } from '../utilities/update-product-tally.utility'
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class CartService {
-    constructor() {}
+    private value: ProductTally[] = []
+
+    constructor() {
+        this.value = []
+    }
+
+    findProduct(id: string): ProductTally {
+        return findProductInCollection(id, this.value)
+    }
+
+    addProduct(id: string): void {
+        if (!this.findProduct(id)) {
+            this.value.push({
+                id: id,
+                tally: 1
+            })
+        } else {
+            this.value = updateProductTally(id, true, this.value)
+        }
+    }
+
+    removeProduct(id: string): void {
+        this.value = updateProductTally(id, false, this.value)
+    }
+
+    reset(): void {
+        this.value = []
+    }
+
+    getValue(): ProductTally[] {
+        return this.value
+    }
 }
--- src/app/services/stock.service.ts
+++ src/app/services/stock.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core'
+import { ProductTally } from '../interfaces/product-tally.interface'
+import { createInitialStock } from '../utilities/create-initial-stock.utility'
+import { findProductInCollection } from '../utilities/find-product-in-collection.utility'
+import { updateProductTally } from '../utilities/update-product-tally.utility'
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class StockService {
-    constructor() {}
+    private value: ProductTally[] = []
+
+    constructor() {
+        this.value = createInitialStock()
+    }
+
+    findProduct(id: string): ProductTally {
+        return findProductInCollection(id, this.value)
+    }
+
+    addProduct(id: string): void {
+        this.value = updateProductTally(id, true, this.value)
+    }
+
+    removeProduct(id: string): void {
+        this.value = updateProductTally(id, false, this.value)
+    }
+
+    reset(): void {
+        this.value = createInitialStock()
+    }
+
+    getValue(): ProductTally[] {
+        return this.value
+    }
 }
